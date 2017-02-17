e71871c88ca5324d3e69d2efa6f11b51583fe126
Michael Luo <deltameter@gmail.com>
2017-02-17 00:38:59 -0800

    backprop started, not working

--- simplenn.py
+++ simplenn.py
@@ -5,7 +5,11 @@ import math
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
+def sigmoid_deriv(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 sigmoid = np.vectorize(sigmoid)
+sigmoid_deriv = np.vectorize(sigmoid_deriv)
 
 def encode_to_onehot(output):
     output = np.eye(output.shape[1])[list(np.argmax(output, 1))]
@@ -19,13 +23,12 @@ class SimpleNN():
     # HYPER PARAMETERS
     # Each entry represents a layer of size n
     # These are just defaults, can be set train() function as well
-    HIDDEN_LAYERS = [200, 200]
+    HIDDEN_LAYERS = [32, 32]
     NUM_EPOCHS = 50
     BATCH_SIZE = 100
-    LEARNING_RATE = 0.01
+    LEARNING_RATE = 0.1
 
     # Each entry represents a layer of size n
-    HIDDEN_LAYERS = [200, 200]
     INPUT_SIZE = 784
     OUTPUT_LAYER_SIZE = 10
 
@@ -52,7 +55,12 @@ class SimpleNN():
         self.train_data = train_dataset['train_images']
         self.train_labels = train_dataset['train_labels']
         self.test_data = test_dataset['test_images']
-        self.test_labels = test_dataset['test_images']
+
+        # didn't come with labels?
+        # self.test_labels = test_dataset['test_labels']
+
+        # labels to one hot
+        self.train_labels = np.eye(self.OUTPUT_LAYER_SIZE)[self.train_labels.T.tolist()]
 
         # simple normalization
         self.train_data = (self.train_data / np.amax(self.train_data))
@@ -63,34 +71,80 @@ class SimpleNN():
         self.test_data = np.swapaxes(self.test_data.reshape((self.INPUT_SIZE, self.test_data.shape[2])), 0, 1)
 
     def forward(self, examples):
-        assert examples.shape[0] == self.INPUT_SIZE, 'Input vectors must be of size ' + self.INPUT_SIZE
+        assert examples.shape[0] == self.INPUT_SIZE, 'Input vectors must be of size {0} not size {1}'.format(self.INPUT_SIZE, examples.shape[0])
+
+        # save dot products and activated neurons so we don't have to do so in back prop
+        dots = []
+        activations = [examples]
 
         for i in range(len(self.weights)):
             examples = self.weights[i].dot(examples)
             examples += self.biases[i]
+            dots.append(examples)
             examples = sigmoid(examples)
-            
-        return examples
+            activations.append(examples)
+
+        return examples.T, dots, activations
 
     # the hard part lmao
     def backward(self, examples, labels):       
-        return examples
+        num_examples = examples.shape[1]
+        weight_updates = [np.zeros(w.shape) for w in self.weights]
+        bias_updates = [np.zeros(b.shape) for b in self.biases]
+
+        output, dots, activations = self.forward(examples)
+
+        for ex in range(num_examples):
+            total_error = sum(output[ex]-labels[ex])
+            
+            # p(Error)/p(Neuron) is initially 1
+            neuron_partials = [1] * self.OUTPUT_LAYER_SIZE
+
+            for i in list(range(len(self.weights)))[::-1]:
+                # calculate the partials of Error w/ respect to dot product of weights and inputs
+                dot_partials = sigmoid_deriv(dots[i][:, ex]) * neuron_partials
+
+                # calculate new neuron partial derivatives w/ respect to the neurons behind
+                # this vectorization took me forever to figure out lmao
+                neuron_partials = self.weights[i].T.dot(dot_partials)
+
+                # non vectorized
+                # for j in range(self.weights[i].shape[1]):
+                    # neuron_partials.append(0)
+                    # for k in range(len(dot_partials)):
+                        # neuron_partials[j] += self.weights[i][k, j] * dot_partials[k]
+
+                weight_partials = dot_partials.reshape(len(dot_partials), 1).dot(activations[i][:,ex].reshape(1, len(activations[i][:,ex])))
+                weight_updates[i] += weight_partials
+                bias_updates[i] += dot_partials.reshape(len(dot_partials), 1)
+            #    print(dot_partials)
+        print(weight_updates[2]) 
+        return weight_updates, bias_updates
 
     def train(self, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, learning_rate=LEARNING_RATE):
+        self.display_accuracy()
         for _ in range(epochs):
             # randomize the order
-            random_permutation = np.random.permutation(batch_size)
-            example_data = self.train_data[random_permutation]
-            example_labels = self.train_labels[random_permutation]
+            random_permutation = np.random.permutation(self.train_data.shape[0])
+            self.train_data = self.train_data[random_permutation]
+            self.train_labels = self.train_labels[random_permutation]
 
             # perform minibatch SGD
-            for start in range(0, len(self.train_data), batch_size):
+            for start in range(0, int(len(self.train_data) / 20), batch_size):
+                print(start)
                 end = start + batch_size
-                a = self.backward(example_data[start:end], example_labels[start:end])
-                print(a)
+                weight_updates, bias_updates = self.backward(self.train_data[start:end].T, self.train_labels[start:end])
+                
+                for i in range(len(self.weights)):
+                    self.weights[i] += weight_updates[i] * (learning_rate / batch_size)
+                    self.biases[i] += bias_updates[i] * (learning_rate / batch_size)
 
-    def test(self):
-        error = calc_accuracy(encode_to_onehot(self.forward(self.test_data)), self.test_labels)
-        print('Test error of {0}'.format(error)) 
+            self.display_accuracy()
+
+        print('Training finished.')
+
+    def display_accuracy(self):
+        error = calc_accuracy(encode_to_onehot(self.forward(self.train_data.T)[0]), self.train_labels)
+        print('Training error of {0}'.format(error)) 
         return error
 
