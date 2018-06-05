3006e255912bdf1f7e5ef254c005e8bdd5e124f5
Michael Luo <deltameter@gmail.com>
2018-06-05 01:06:03 -0700

    little work on rnn

--- layers.py
+++ layers.py
@@ -11,11 +11,11 @@ class FullyConnected(Layer):
         self._weights = np.array(np.random.rand(input_size, output_size) - 0.5) * init_weights_size
         self._biases = np.zeros(output_size)
         self._nonlinearity = nonlinearity
-        self._cached_prev_activations = None
+        self._cached_inputs = None
         self._cached_activations = None
 
-    def forward(self, prev_activations, cache=False):
-        linear = prev_activations.dot(self._weights) + self._biases
+    def forward(self, inputs, cache=False):
+        linear = inputs.dot(self._weights) + self._biases
 
         # apply nonlinearity
         if self._nonlinearity is not None:
@@ -26,7 +26,7 @@ class FullyConnected(Layer):
         # cache these for backpropagation
         if cache:
             self._cached_sum = linear
-            self._cached_prev_activations = prev_activations
+            self._cached_inputs = prev_activations
             self._cached_activations = activations
 
         return activations
@@ -44,10 +44,10 @@ class FullyConnected(Layer):
             grad_wrt_sum = out_errors
         # calculate error wrt weights
         # get gradient by accumulating gradient for each training ex.
-        grad_wrt_weights = self._cached_prev_activations.T.dot(grad_wrt_sum)
+        grad_wrt_weights = self._cached_inputs.T.dot(grad_wrt_sum)
         grad_wrt_biases = grad_wrt_sum.sum(axis=0)
 
-        # calculate error wrt prev output, 1 row for each example
+        # calculate error wrt inputs, 1 row for each example
         in_errors = grad_wrt_sum.dot(self._weights.T)
 
         compensated_lr = lr / batch_size
@@ -55,3 +55,41 @@ class FullyConnected(Layer):
         self._biases -= grad_wrt_biases * compensated_lr
          
         return in_errors
+
+class Recurrent(Layer):
+
+    def __init__(self, input_size, output_size, init_weights_size, nonlinearity):
+        self._weights_input = np.array(np.random.rand(input_size, output_size) - 0.5) * init_weights_size
+        self._weights_hidden = np.array(np.random.rand(output_size, output_size) - 0.5) * init_weights_size
+        self._biases = np.zeros(output_size)
+
+        self._nonlinearity = nonlinearity
+        self._hidden_states = []
+
+    def forward(self, inputs, cache=False):
+        if len(self._hidden_states) == 0:
+            # seed with 0 vectors of size n x output
+            last_state = self.zeros((len(inputs), len(self.weights_output[0])))
+        else:
+            last_state = self._hidden_states[-1]
+
+        h_t = self.inputs.dot(self._weights_input) + last_state.dot(self._weights_output) + self.biases
+        
+        if self.cache:
+            self._hidden_states.append(h_t)
+        else:
+            self._hidden_states[-1] = h_t
+
+    def backprop(self, out_errors, lr):
+        '''
+        out errors - tuple (t, errors) dL/dh_t 
+        '''
+        
+        d_w_input = np.zeros(self._weights_input.shape)
+        d_w_hidden = np.zeros(self._weights_hidden.shape)
+
+        for e in out_errors:
+            for i in reversed(range(t + 1)):
+                g = self._nonlinearity.gradient(self._hidden_states[i])
+
+
